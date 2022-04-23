46bf493610dbda7581eb3050410c1bd28270f745
sebasoba <76491101+sebasoba@users.noreply.github.com>
2022-04-23 00:14:27 -0700

    Add files via upload

---
+++ p147.js
@@ -0,0 +1,30 @@
+<html>
+<head>
+<script src="https://aframe.io/releases/0.6.0/aframe.min.js"> </script> 
+</head>
+<body>
+
+<a-scene>
+<a-plane
+    id="wall-poster-right"
+    src="https://ipinimg.com/originals/22/cd/76/22cd76da45c34cffee67dbca77062b5.png"
+    position="0 0 0"
+    rotation="0 0 0"
+    width="4"
+    height="6"
+></a-plane>
+
+<a-cube
+    color="#000"
+    height="1"
+    width="1"
+    position="0 1 0"
+
+></a-cube>
+
+
+
+
+</a-scene>
+</body>
+</html>
